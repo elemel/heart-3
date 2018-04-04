992cccbdfd93e79ac47f56d5c1d25093f7e79f97
Mikael Lind <elemel@spotify.com>
2018-04-04 21:24:13 +0200

    Clean up math utils

--- src/heart/math/utils.lua
+++ src/heart/math/utils.lua
@@ -1,31 +1,37 @@
-local mathUtils = {}
+local acos = math.acos
+local cos = math.cos
+local max = math.max
+local min = math.min
+local pi = math.pi
+local sin = math.sin
+local sqrt = math.sqrt
 
-function mathUtils.sign(x)
+function sign(x)
   return x < 0 and -1 or 1
 end
 
-function mathUtils.clamp(x, x1, x2)
-  return math.min(math.max(x, x1), x2)
+function clamp(x, x1, x2)
+  return min(max(x, x1), x2)
 end
 
-function mathUtils.length2(x, y)
-  return math.sqrt(x * x + y * y)
+function length2(x, y)
+  return sqrt(x * x + y * y)
 end
 
-function mathUtils.distance2(x1, y1, x2, y2)
-  return math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+function distance2(x1, y1, x2, y2)
+  return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
 end
 
-function mathUtils.dot2(x1, y1, x2, y2)
+function dot2(x1, y1, x2, y2)
   return x1 * x2 + y1 * y2
 end
 
-function mathUtils.cross2(x1, y1, x2, y2)
+function cross2(x1, y1, x2, y2)
   return x1 * y2 - x2 * y1
 end
 
-function mathUtils.normalize2(x, y)
-  local length = mathUtils.length2(x, y)
+function normalize2(x, y)
+  local length = length2(x, y)
 
   if length == 0 then
     return 1, 0, 0
@@ -34,66 +40,62 @@ function mathUtils.normalize2(x, y)
   return x / length, y / length, length
 end
 
-function mathUtils.clampLength2(x, y, minLength, maxLength)
-  local x, y, length = mathUtils.normalize2(x, y)
-  local clampedLength = mathUtils.clamp(length, minLength, maxLength)
+function clampLength2(x, y, minLength, maxLength)
+  local x, y, length = normalize2(x, y)
+  local clampedLength = clamp(length, minLength, maxLength)
   return x * clampedLength, y * clampedLength, length
 end
 
-function mathUtils.mix(x1, x2, t)
+function mix(x1, x2, t)
   return (1 - t) * x1 + t * x2
 end
 
-function mathUtils.mix2(x1, y1, x2, y2, t)
+function mix2(x1, y1, x2, y2, t)
   return (1 - t) * x1 + t * x2, (1 - t) * y1 + t * y2
 end
 
-function mathUtils.normalizeAngle(a)
-  return (a + math.pi) % (2 * math.pi) - math.pi
+function normalizeAngle(a)
+  return (a + pi) % (2 * pi) - pi
 end
 
-function mathUtils.mixAngles(a1, a2, t)
-  return a1 + mathUtils.normalizeAngle(a2 - a1) * t
+function mixAngles(a1, a2, t)
+  return a1 + normalizeAngle(a2 - a1) * t
 end
 
-function mathUtils.rotate2(x, y, angle)
-  local cosAngle = math.cos(angle)
-  local sinAngle = math.sin(angle)
+function rotate2(x, y, angle)
+  local cosAngle = cos(angle)
+  local sinAngle = sin(angle)
   return cosAngle * x + -sinAngle * y, sinAngle * x + cosAngle * y
 end
 
-function mathUtils.toLocalPoint2(
-  worldX, worldY, parentX, parentY, parentAngle)
-
+function toLocalPoint2(worldX, worldY, parentX, parentY, parentAngle)
   local rotatedX = worldX - parentX
   local rotatedY = worldY - parentY
-  local localX, localY = mathUtils.rotate2(rotatedX, rotatedY, -parentAngle)
+  local localX, localY = rotate2(rotatedX, rotatedY, -parentAngle)
   return localX, localY
 end
 
-function mathUtils.toWorldPoint2(
-  localX, localY, parentX, parentY, parentAngle)
-
-  local rotatedX, rotatedY = mathUtils.rotate2(localX, localY, parentAngle)
+function toWorldPoint2(localX, localY, parentX, parentY, parentAngle)
+  local rotatedX, rotatedY = rotate2(localX, localY, parentAngle)
   local worldX = rotatedX + parentX
   local worldY = rotatedY + parentY
   return worldX, worldY
 end
 
-function mathUtils.toLocalTransform2(
+function toLocalTransform2(
   worldX, worldY, worldAngle, parentX, parentY, parentAngle)
 
   local rotatedX = worldX - parentX
   local rotatedY = worldY - parentY
-  local localX, localY = mathUtils.rotate2(rotatedX, rotatedY, -parentAngle)
+  local localX, localY = rotate2(rotatedX, rotatedY, -parentAngle)
   local localAngle = worldAngle - parentAngle
   return localX, localY, localAngle
 end
 
-function mathUtils.toWorldTransform2(
+function toWorldTransform2(
   localX, localY, localAngle, parentX, parentY, parentAngle)
 
-  local rotatedX, rotatedY = mathUtils.rotate2(localX, localY, parentAngle)
+  local rotatedX, rotatedY = rotate2(localX, localY, parentAngle)
   local worldX = rotatedX + parentX
   local worldY = rotatedY + parentY
   local worldAngle = localAngle + parentAngle
@@ -101,7 +103,7 @@ function mathUtils.toWorldTransform2(
 end
 
 -- http://frederic-wang.fr/decomposition-of-2d-transform-matrices.html
-function mathUtils.decompose2(transform)
+function decompose2(transform)
   local t11, t12, t13, t14,
     t21, t22, t23, t24,
     t31, t32, t33, t34,
@@ -120,13 +122,13 @@ function mathUtils.decompose2(transform)
 
     if scaleX >= scaleY then
       skewX = (t11 * t12 + t21 * t22) / scaleX
-      scaleX = math.sqrt(scaleX)
-      angle = mathUtils.sign(t11) * math.acos(t11 / scaleX)
+      scaleX = sqrt(scaleX)
+      angle = sign(t11) * acos(t11 / scaleX)
       scaleY = det / scaleX
     else
       skewY = (t11 * t12 + t21 * t22) / scaleY
-      scaleY = math.sqrt(scaleY)
-      angle = 0.5 * math.pi - mathUtils.sign(t22) * math.acos(-t12 / scaleY)
+      scaleY = sqrt(scaleY)
+      angle = 0.5 * pi - sign(t22) * acos(-t12 / scaleY)
       scaleX = det / scaleY
     end
   end
@@ -134,4 +136,23 @@ function mathUtils.decompose2(transform)
   return x, y, angle, scaleX, scaleY, 0, 0, skewX, skewY
 end
 
-return mathUtils
+return {
+  clamp = clamp,
+  clampLength2 = clampLength2,
+  cross2 = cross2,
+  decompose2 = decompose2,
+  distance2 = distance2,
+  dot2 = dot2,
+  length2 = length2,
+  mix = mix,
+  mix2 = mix2,
+  mixAngles = mixAngles,
+  normalize2 = normalize2,
+  normalizeAngle = normalizeAngle,
+  rotate2 = rotate2,
+  sign = sign,
+  toLocalPoint2 = toLocalPoint2,
+  toLocalTransform2 = toLocalTransform2,
+  toWorldPoint2 = toWorldPoint2,
+  toWorldTransform2 = toWorldTransform2,
+}
